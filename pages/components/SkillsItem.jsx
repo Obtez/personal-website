745e4650ba7b8c22f94005ed2e81b210656d0010
Obtez <59846143+Obtez@users.noreply.github.com>
2021-11-25 21:58:16 +0100

    added a conditional in skillsItem to not render if the property doesnt exist

--- pages/components/SkillsItem.jsx
+++ pages/components/SkillsItem.jsx
@@ -1,7 +1,12 @@
 import Image from "next/image"
 import styles from "../../styles/Skills.module.scss";
 
-const SkillsItem = ({skill}) => {
+const SkillsItem = ({ skill }) => {
+  
+  if (!skill) {
+    return null;
+  }
+
   return (
     <li className={styles.skillItem}>
         <Image src={`/assets/skills-icons/${skill.url}_icon.svg`} alt={skill.text} width={100} height={100} />
---
+++ pages/components/data/_constants.js
@@ -0,0 +1,50 @@
+const skillsList = [
+  {
+    url: "html",
+    text: "HTML"
+  },
+  {
+    url: "css",
+    text: "CSS"
+  },
+  {
+    url: "sass",
+    text: "SCSS"
+  },
+  {
+    url: "js",
+    text: "JavaScript"
+  },
+   {
+    url: "typescript",
+    text: "TypeScript"
+  },
+  {
+    url: "react",
+    text: "React"
+  },
+  {
+    url: "nodejs",
+    text: "Node.js" 
+  },
+  {
+    url: "nextjs",
+    text: "Next.js"
+  },
+  {
+    url: "mongodb",
+    text: "MongoDB"
+  },
+  {
+    url: "git",
+    text: "Git"
+  },
+  {
+    url: "github",
+    text: "GitHub"
+  }
+]
+
+export {
+  skillsList
+}
