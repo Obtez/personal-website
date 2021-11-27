ddedde85e3eea8ff5e5796d8b14019e2c8854c38
Obtez <59846143+Obtez@users.noreply.github.com>
2021-11-27 19:19:25 +0100

    project tech -> project modal

--- pages/components/projectModal/ProjectHeader.jsx
+++ pages/components/projectModal/ProjectHeader.jsx
@@ -9,7 +9,6 @@ const ProjectHeader = ({ project }) => {
     <header>
       <h3 className={styles.projectTitle}>{project.title}</h3>
       <a href={project.demoURL} target="_blank" rel="noreferrer" className={`${styles.secondaryBtn} ${styles.demoBtn} ${styles.demoBtnMobile}`}><FaPlayCircle /> Demo</a>
-      <p className={styles.projectTech}>{project.tech}</p>
     </header>
   )
 };
